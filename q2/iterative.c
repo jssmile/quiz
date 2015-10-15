898acbbf4a652e735ca8b2897256422239b7b119
jssmile <jssmile19921112@gmail.com>
2015-10-15 12:05:34 +0800

    Complete the q2 version1. Not test for the running time, cache miss...etc

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,21 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+
+char smallest_character(char str[], char c)
+{
+    for (int i=0; i<sizeof(str); i++)
+        if(str[i]>c)return str[i];
+    return str[0];
+}
 
 int main()
 {
+    char str[10];
+    char c;
+    printf("Enter the SORTED string : ");
+    gets(str);
+    printf("Enter the search character : ");
+    scanf("%c",&c);
+    fflush(stdin);
+    printf("%c\n",smallest_character(str, c));
     return 0;
 }
--- q2/recursive.c
+++ q2/recursive.c
@@ -1,6 +1,26 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+
+char smallest_character(char str[], char c)
+{
+    if (str[0]!='\0') {
+        if (str[0]>c)
+            return str[0];
+        else {
+            return ((smallest_character(str+1,c)<c)?str[0]:smallest_character(str+1,c));
+        }
+        return str[0];
+    }
+}
 
 int main()
 {
+    char str[10];
+    char c;
+    printf("Enter the SORTED string : ");
+    gets(str);
+    printf("Enter the search character : ");
+    scanf("%c",&c);
+    fflush(stdin);
+    printf("%c\n",smallest_character(str, c));
     return 0;
 }
